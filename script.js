67ae3dafebaa0c8ce0fa51e27226b0bc81495ed6
Raylla Silva <rayllasouza.rs@gmail.com>
2021-01-18 17:42:22 -0300

    Adicionando direções e limitando a área de movimento da cobrinha

--- script.js
+++ script.js
@@ -20,7 +20,21 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener("keydown",update); //reconhece o evento de clique
+
+function update (event){
+    if(event.keyCode == 37 && direction != "rigth"){direction="left"}
+    if(event.keyCode == 38 && direction != "down"){direction="up"}
+    if(event.keyCode == 39 && direction != "left"){direction="rigth"}
+    if(event.keyCode == 40 && direction != "up"){direction="down"}
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15*box && direction == "right"){snake[0].x=0}
+    if(snake[0].x < 0 && direction == "left"){snake[0].x=16}
+    if(snake[0].y > 15*box && direction == "down"){snake[0].y=0}
+    if(snake[0].y < 0 && direction == "up"){snake[0].y=16}
+
     criarBG();
     criarCobrinha();
 
