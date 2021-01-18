78832517fd843914425eff9b05891f873f5a00dd
Raylla Silva <rayllasouza.rs@gmail.com>
2021-01-18 18:16:37 -0300

    Definindo aleatoriedade da comidinha e tamanho da cobrinha

--- script.js
+++ script.js
@@ -32,16 +32,16 @@ function drawFood(){
 document.addEventListener("keydown",update); //reconhece o evento de clique
 
 function update (event){
-    if(event.keyCode == 37 && direction != "rigth"){direction="left"}
+    if(event.keyCode == 37 && direction != "right"){direction="left"}
     if(event.keyCode == 38 && direction != "down"){direction="up"}
-    if(event.keyCode == 39 && direction != "left"){direction="rigth"}
+    if(event.keyCode == 39 && direction != "left"){direction="right"}
     if(event.keyCode == 40 && direction != "up"){direction="down"}
 }
 
 function iniciarJogo(){
-    if(snake[0].x > 15*box && direction == "right"){snake[0].x=0}
+    if(snake[0].x > (15*box) && direction == "right"){snake[0].x=0}
     if(snake[0].x < 0 && direction == "left"){snake[0].x=16}
-    if(snake[0].y > 15*box && direction == "down"){snake[0].y=0}
+    if(snake[0].y > (15*box) && direction == "down"){snake[0].y=0}
     if(snake[0].y < 0 && direction == "up"){snake[0].y=16}
 
     criarBG();
@@ -57,7 +57,13 @@ function iniciarJogo(){
     if(direction == "up"){snakeY-=box;}
     if(direction == "down"){snakeY+=box;}
 
-    snake.pop();
+    if(snakeX!=food.x || snakeY !=food.y){
+        snake.pop();
+    }
+    else{
+        food.x= Math.floor(Math.random()* 15 +1)*box;
+        food.y= Math.floor(Math.random()* 15 +1)*box;
+    }
 
     let newHead = {
         x: snakeX,
